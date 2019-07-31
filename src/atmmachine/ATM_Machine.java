d9db9cbf353d96b63c44f7a81623e5dd45cfa8e8
srkamath <srkamath@srkamath-l>
2019-07-31 15:26:44 +0530

    beta 1.3

--- src/atmmachine/ATM_Machine.java
+++ src/atmmachine/ATM_Machine.java
@@ -11,7 +11,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.Date;
-
+//hi
 
 public class ATM_Machine {
 	String Card_number;//Card number
